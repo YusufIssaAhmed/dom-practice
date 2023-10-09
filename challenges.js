f4c0cffb5ef815e7ee84b5f484d2576787c48dd9
YusufIssaAhmed <yusuf99856@gmail.com>
2023-10-09 20:16:53 +0100

    function and do while

--- challenges.js
+++ challenges.js
@@ -1,36 +1,63 @@
-for(let i = 0; i < 20; i++) {
-    console.log("Ahmed")
-}
+// for(let i = 0; i < 20; i++) {
+//     console.log("Yusuf")
+// }
 
 
-
-let i = 0;
-while (i < 20) {
-    console.log("Dini");
-    i++;
-}
-
-
-let today = "monday";
-
-if(today == "Monday") {
-    console.log("It is Coding Day")
-} else if (today == "Tuesday") {
-    console.log("It is my off day")
-} else {
-    console.log("ask me personally which day")
-}
+// let i = 0;
+// while (i < 20) {
+//     console.log("Ahmed");
+//     i++;
+// }
 
 
-switch (today) {
-    case "Monday":
-        console.log("It is Coding Day, from switch")
-        break;
-    case "Tuesday":
-        console.log("It is my off day, from switch")
-        break;
+// let today = "monday";
 
-    default:
-        console.log("ask me personally which day, from switch")
-        break;
-}
+// if(today == "Monday") {
+//     console.log("It is Coding Day")
+// } else if (today == "Tuesday") {
+//     console.log("It is my off day")
+// } else {
+//     console.log("ask me personally which day")
+// }
+
+
+// switch (today) {
+//     case "Monday":
+//         console.log("It is Coding Day, from switch")
+//         break;
+//     case "Tuesday":
+//         console.log("It is my off day, from switch")
+//         break;
+
+//     default:
+//         console.log("ask me personally which day, from switch")
+// //         break;
+// }
+
+
+// let i = 0;
+// do {
+//     console.log("yusuf from do while");
+//     i++;
+// }
+// while (i < 5)
+
+// function multiplyAndPrint(num1, num2) {
+//     const result = num1 * num2;
+//     console.log("The result of multiplying", num1, "and", num2, "is:", result);
+//     return result;
+//   }
+  
+//   // Example usage:
+//   const number1 = 5;
+//   const number2 = 3;
+//   const multiplicationResult = multiplyAndPrint(number1, number2);
+  
+
+
+function multiply (num1,num2) {
+    let result = num1*num2;
+    return result;
+
+
+    let sum = multiply (5,6)
